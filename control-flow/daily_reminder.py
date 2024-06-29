2f7ab44dea45a326f3a5ec2bc7d4cf1a5af1e630
Firass Zaoui <109809162+Picuman@users.noreply.github.com>
2024-06-29 01:03:15 +0100

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,3 +1,15 @@
 task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
+
+match priority:
+    case "high":
+        print(f"**high priority task:** {task}")
+    case "medium":
+        print(f"**medium priority task:** {task}")
+    case "low":
+        print(f"**low priority task:** {task}")
+    case _:
+        print(" Invalid priority level entered.")
+        
+
