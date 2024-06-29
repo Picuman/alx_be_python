c3395df8d52ada4346194e9244226d0dc597cd95
Firass Zaoui <109809162+Picuman@users.noreply.github.com>
2024-06-29 01:13:03 +0100

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -11,5 +11,9 @@ match priority:
         print(f"**low priority task:** {task}")
     case _:
         print(" Invalid priority level entered.")
-        
+if time_bound == "yes":
+    print(f"**Reminder:** '{task}' is a high prority task that requires immediatee attention today!")
+else:
+    print(f"**Note:** '{task}' is a low priority task. Consider completing it when you have free time.")
+  
 
